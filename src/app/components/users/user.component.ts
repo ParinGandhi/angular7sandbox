b48954e3510a907146dab64d64104687399134d1
Parin Gandhi <parin.a.gandhi@gmail.com>
2018-12-22 00:00:22 -0500

    Using types

--- src/app/components/users/user.component.ts
+++ src/app/components/users/user.component.ts
@@ -8,18 +8,39 @@ import { Component } from '@angular/core';
 
 export class UserComponent {
     // Properties
-    firstName = 'John';
-    lastName = 'Smith';
-    age = 30;
-    address = {
-        street: '50 Main St.',
-        city: 'Boston',
-        state: 'MA'
+    firstName: string;
+    lastName: string;
+    age: number;
+    address;
+
+    // Different 'types' in TypeScript
+    foo: any;
+    hasKids: boolean;
+    numberArray: number[];
+    stringArray: string[];
+    mixedArray: any[];
+    myTuple: [string, number, boolean];
+    unusable: void;
+    u: undefined;
+    n: null;
+
+    // Taking in 2 numbers and returning a number type
+    addNumber(num1: number, num2: number): number {
+        return num1 + num2;
     }
+
 
     // Methods
     // Constructor will run when this class is initialized
     constructor() {
+        this.firstName = 'John';
+        this.lastName = 'Doe';
+        this.age = 30;
+        this.address = {
+            street: '50 Main St.',
+            city: 'Boston',
+            state: 'MA'
+        };
 
     }
 }
